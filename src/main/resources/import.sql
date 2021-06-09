7d6f7eb24cdfe8d87efbbb40d1832cccb8ba404e
Shahid Hasan <shahid_hasan@epam.com>
2021-06-09 11:19:31 +0530

    Loading static user data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into USER values(101, 'mohitkumar@gmail.com','Mohit', 'Kumar', 'admin', 'ssn101','mohitkumar')
+insert into USER values(102, 'gauravsingh@gmail.com','Gaurav', 'Singh', 'admin', 'ssn102','gauravsingh')
+insert into USER values(103, 'arvingarora@gmail.com','Arvind', 'Arora', 'admin', 'ssn103','arvindarora')
